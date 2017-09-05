2a50632f3490e8c77f81a9a7ed7aa67b9f568354
Tom Stevens <TStevens@arl.local>
2017-09-05 17:15:09 -0400

    added onkeypress function, assigned var to div, need to fix current word fill.

---
+++ assets/logic.js
@@ -0,0 +1,12 @@
+// Array of Potential Words//
+ 
+ 	var PotWords = [
+ 	"Chicken Wings", "Hamburger",
+ 	"Chicken Parm", "Filet Mignion"
+ 	"Chicken Cordon Bleu","Fish & Chips"
+ 	, "Shrimp", "Sushi", "Crab", "Lobster"
+ 	];
+
+
+ 	console.log( for (var i = 0; i < PotWords.length; i++) {
+        console.log("I love " + PotWords[i]);)
